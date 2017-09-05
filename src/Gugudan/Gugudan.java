932260796709bf7f0341da07f49649398bc432ba
김수진 <@DESKTOP-GE58HHE>
2017-09-05 16:58:19 +0900

    times table 2

---
+++ src/Gugudan/Gugudan.java
@@ -0,0 +1,19 @@
+package Gugudan;
+
+public class Gugudan {
+	public static void main(String[] args) {
+		System.out.print("2*1");
+		System.out.println("=2");
+		System.out.print("2*2");
+		System.out.println("=4");
+		System.out.print("2*3");
+		System.out.println("=6");
+		System.out.print("2*4");
+		System.out.println("=8");
+		System.out.print("2*5");
+		System.out.println("=10");
+		System.out.print("2*6");
+		System.out.println("=12");
+	}
+
+}
